4ba43d551c3af83364b41b53977b497d476af1ac
theawsomeavr <32213630+theawsomeavr@users.noreply.github.com>
2019-03-11 00:16:27 -0700

    Add files via upload

---
+++ tetris_lcd/LedControl_for_tetris.cpp
@@ -0,0 +1,274 @@
+/*
+ *    LedControl.cpp - A library for controling Leds with a MAX7219/MAX7221
+ *    Copyright (c) 2007 Eberhard Fahle
+ * 
+ *    Permission is hereby granted, free of charge, to any person
+ *    obtaining a copy of this software and associated documentation
+ *    files (the "Software"), to deal in the Software without
+ *    restriction, including without limitation the rights to use,
+ *    copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *    copies of the Software, and to permit persons to whom the
+ *    Software is furnished to do so, subject to the following
+ *    conditions:
+ * 
+ *    This permission notice shall be included in all copies or 
+ *    substantial portions of the Software.
+ * 
+ *    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ *    EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ *    OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ *    NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ *    HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ *    WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ *    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ *    OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+
+#include "LedControl_for_tetris.h"
+#include "Arduino.h"
+//the opcodes for the MAX7221 and MAX7219
+#define OP_NOOP   0
+#define OP_DIGIT0 1
+#define OP_DIGIT1 2
+#define OP_DIGIT2 3
+#define OP_DIGIT3 4
+#define OP_DIGIT4 5
+#define OP_DIGIT5 6
+#define OP_DIGIT6 7
+#define OP_DIGIT7 8
+#define OP_DECODEMODE  9
+#define OP_INTENSITY   10
+#define OP_SCANLIMIT   11
+#define OP_SHUTDOWN    12
+#define OP_DISPLAYTEST 15
+
+LedControl::LedControl(int dataPin, int clkPin, int csPin, int numDevices) {
+    SPI_MOSI=dataPin;
+    SPI_CLK=clkPin;
+    SPI_CS=csPin;
+    if(numDevices<=0 || numDevices>8 )
+        numDevices=8;
+    maxDevices=numDevices;
+    pinMode(SPI_MOSI,OUTPUT);
+    pinMode(SPI_CLK,OUTPUT);
+    pinMode(SPI_CS,OUTPUT);
+    digitalWrite(SPI_CS,HIGH);
+    SPI_MOSI=dataPin;
+    for(int i=0;i<64;i++) 
+        status[i]=0x00;
+    for(int i=0;i<maxDevices;i++) {
+        spiTransfer(i,OP_DISPLAYTEST,0);
+        //scanlimit is set to max on startup
+        setScanLimit(i,7);
+        //decode is done in source
+        spiTransfer(i,OP_DECODEMODE,0);
+        clearDisplay(i);
+        //we go into shutdown-mode on startup
+        shutdown(i,true);
+    }
+	for (int i=0; i<80; i++)
+		buffer[i] = 0;
+}
+
+int LedControl::getDeviceCount() {
+    return maxDevices;
+}
+
+void LedControl::shutdown(int addr, bool b) {
+    if(addr<0 || addr>=maxDevices)
+        return;
+    if(b)
+        spiTransfer(addr, OP_SHUTDOWN,0);
+    else
+        spiTransfer(addr, OP_SHUTDOWN,1);
+}
+
+void LedControl::setScanLimit(int addr, int limit) {
+    if(addr<0 || addr>=maxDevices)
+        return;
+    if(limit>=0 && limit<8)
+        spiTransfer(addr, OP_SCANLIMIT,limit);
+}
+
+void LedControl::setIntensity(int addr, int intensity) {
+    if(addr<0 || addr>=maxDevices)
+        return;
+    if(intensity>=0 && intensity<16)	
+        spiTransfer(addr, OP_INTENSITY,intensity);
+}
+
+void LedControl::clearDisplay(int addr) {
+    int offset;
+
+    if(addr<0 || addr>=maxDevices)
+        return;
+    offset=addr*8;
+    for(int i=0;i<8;i++) {
+        status[offset+i]=0;
+        spiTransfer(addr, i+1,status[offset+i]);
+    }
+}
+
+void LedControl::setLed(byte col, byte row, byte value)
+{
+    bitWrite(buffer[col], row, value);
+
+	int n = col / 8;
+	int c = col % 8;
+	digitalWrite(SPI_CS, LOW);    
+	for (int i=0; i<maxDevices; i++) 
+	{
+		if (i == n)
+		{
+			shiftOut(SPI_MOSI, SPI_CLK, MSBFIRST, c + 1);
+			shiftOut(SPI_MOSI, SPI_CLK, MSBFIRST, buffer[col]);
+		}
+		else
+		{
+			shiftOut(SPI_MOSI, SPI_CLK, MSBFIRST, 0);
+			shiftOut(SPI_MOSI, SPI_CLK, MSBFIRST, 0);
+		}
+	}
+	digitalWrite(SPI_CS, LOW);
+	digitalWrite(SPI_CS, HIGH);
+}
+
+void LedControl::setRow(int addr, int row, byte value) {
+    int offset;
+    if(addr<0 || addr>=maxDevices)
+        return;
+    if(row<0 || row>7)
+        return;
+    offset=addr*8;
+    status[offset+row]=value;
+    spiTransfer(addr, row+1,status[offset+row]);
+}
+
+
+void LedControl::shiftLeft(bool rotate, bool fill_zero)
+{
+	byte old = buffer[0];
+	int i;
+	for (i=0; i<80; i++)
+		buffer[i] = buffer[i+1];
+	if (rotate) buffer[maxDevices*8-1] = old;
+	else if (fill_zero) buffer[maxDevices*8-1] = 0;
+	
+	for (int i=0; i<8; i++)
+	{
+		int col = i;
+		digitalWrite(SPI_CS, LOW);    
+		for (int j=0; j<maxDevices; j++) 
+		{
+			shiftOut(SPI_MOSI, SPI_CLK, MSBFIRST, i + 1);
+			shiftOut(SPI_MOSI, SPI_CLK, MSBFIRST, buffer[col]);
+			col += 8;
+		}
+		digitalWrite(SPI_CS, LOW);
+		digitalWrite(SPI_CS, HIGH);
+	}
+}
+void LedControl::writeSprite(int x, int y, const byte* sprite)
+{
+	int w = sprite[0];
+	int h = sprite[1];
+	
+	if (h == 8 && y == 0)
+		for (int i=0; i<w; i++)
+		{
+			int c = x + i;
+			if (c>=0 && c<80)
+				setColumn(c, sprite[i+2]);
+		}
+	else
+		for (int i=0; i<w; i++)
+			for (int j=0; j<h; j++)
+			{
+				int c = x + i;
+				int r = y + j;
+				if (c>=0 && c<80 && r>=0 && r<8)
+					setLed(c, r, bitRead(sprite[i+2], j));
+			}
+}
+void LedControl::setColumn(byte col, byte value)
+{
+	int n = col / 8;
+	int c = col % 8;
+	digitalWrite(SPI_CS, LOW);    
+	for (int i=0; i<maxDevices; i++) 
+	{
+		if (i == n)
+		{
+			shiftOut(SPI_MOSI, SPI_CLK, MSBFIRST, c + 1);
+			shiftOut(SPI_MOSI, SPI_CLK, MSBFIRST, value);
+		}
+		else
+		{
+			shiftOut(SPI_MOSI, SPI_CLK, MSBFIRST, 0);
+			shiftOut(SPI_MOSI, SPI_CLK, MSBFIRST, 0);
+		}
+	}
+	digitalWrite(SPI_CS, LOW);
+	digitalWrite(SPI_CS, HIGH);
+	
+	buffer[col] = value;
+}
+
+void LedControl::setDigit(int addr, int digit, byte value, boolean dp) {
+    int offset;
+    byte v;
+
+    if(addr<0 || addr>=maxDevices)
+        return;
+    if(digit<0 || digit>7 || value>15)
+        return;
+    offset=addr*8;
+    v=pgm_read_byte_near(charTable + value); 
+    if(dp)
+        v|=B10000000;
+    status[offset+digit]=v;
+    spiTransfer(addr, digit+1,v);
+}
+
+void LedControl::setChar(int addr, int digit, char value, boolean dp) {
+    int offset;
+    byte index,v;
+
+    if(addr<0 || addr>=maxDevices)
+        return;
+    if(digit<0 || digit>7)
+        return;
+    offset=addr*8;
+    index=(byte)value;
+    if(index >127) {
+        //no defined beyond index 127, so we use the space char
+        index=32;
+    }
+    v=pgm_read_byte_near(charTable + index); 
+    if(dp)
+        v|=B10000000;
+    status[offset+digit]=v;
+    spiTransfer(addr, digit+1,v);
+}
+
+void LedControl::spiTransfer(int addr, volatile byte opcode, volatile byte data) {
+    //Create an array with the data to shift out
+    int offset=addr*2;
+    int maxbytes=maxDevices*2;
+
+    for(int i=0;i<maxbytes;i++)
+        spidata[i]=(byte)0;
+    //put our device data into the array
+    spidata[offset+1]=opcode;
+    spidata[offset]=data;
+    //enable the line 
+    digitalWrite(SPI_CS,LOW);
+    //Now shift out the data 
+    for(int i=maxbytes;i>0;i--)
+        shiftOut(SPI_MOSI,SPI_CLK,MSBFIRST,spidata[i-1]);
+    //latch the data onto the display
+    digitalWrite(SPI_CS,HIGH);
+}    
+
+
---
+++ tetris_lcd/tetris_lcd.ino
@@ -0,0 +1,1082 @@
+/**********************************************************
+ * This is a version of annejan´s tetris code https://github.com/IJHack/tetris modified by David Rubio 
+ * so that you can play tetris on 2 lcds(annejan´s tetris code) and at the same time you are hearing 
+ * the tetris theme play by the same MCU/arduino board without any external Integrated Circuits (the music is outputted through pin 9)
+ * and at game over present the current score (number of lines that you have eliminated) as well as the highest score (that is saved on the eeprom).
+ * It is very important the you have wipe the eeprom before you upload this code https://www.arduino.cc/en/Tutorial/EEPROMClear
+ *********************************************************/
+#include "LedControl_for_tetris.h"
+#include "PWM_Playtune_for_tetris.h"
+#include <EEPROM.h>
+#include "charac.h"
+#include "music.h"
+byte buffer[10];
+PWM_Playtune pt;
+long delays = 0;
+short delay_ = 500;
+long bdelay = 0;
+short buttondelay = 200;
+short btdowndelay = 80;
+short btsidedelay = 200;
+unsigned char blocktype;
+unsigned char blockrotation;
+int points;
+boolean  block[8][18]; //2 extra for rotation
+boolean  pile[8][16];
+boolean disp[8][16];
+static const int DATA_PIN = 2;
+static const int CLK_PIN  = 3;
+static const int CS_PIN   = 4;
+static const int DISPLAYS = 2;
+
+LedControl lc = LedControl(DATA_PIN, CLK_PIN, CS_PIN, DISPLAYS);
+
+
+void setup() {
+  int seed =
+    (analogRead(0) + 1) *
+    (analogRead(1) + 1) *
+    (analogRead(2) + 1) *
+    (analogRead(3) + 1);
+  randomSeed(seed);
+  random(10, 9610806);
+  seed = seed * random(3336, 15679912) + analogRead(random(4)) ;
+  randomSeed(seed);
+  random(10, 98046);
+
+  // The MAX72XX is in power-saving mode on startup,
+  // we have to do a wakeup call
+  lc.shutdown(0, false);
+  lc.shutdown(1, false);
+  // Set the brightness
+  lc.setIntensity(0, 10);
+  lc.setIntensity(1, 10);
+  // and clear the display
+  lc.clearDisplay(0);
+  lc.clearDisplay(1);
+  pinMode(5, INPUT_PULLUP);
+  pinMode(8, INPUT_PULLUP);
+  pinMode(6, INPUT_PULLUP);
+  pinMode(7, INPUT_PULLUP);
+  pt.begin();
+  //you can use the funtion pt.set_volume to set the volume of the music, you must input a value between 0-100.
+  //pt.set_volume(50);
+  newBlock();
+  updateLED();
+
+
+}
+
+void loop() {
+
+  if (delays < millis())
+  {
+    delays = millis() + delay_;
+    movedown();
+  }
+
+
+
+  if (!pt.tune_playing) pt.tune_playscore (score);
+
+  //buttun actions
+  int button = readDirection();
+
+  if (button == 1) //up=rotate
+    rotate();
+  if (button == 2) //right=moveright
+    moveright();
+  if (button == 3) //left=moveleft
+    moveleft();
+  if (button == 4) //down=movedown
+    movedown();
+
+
+
+  LEDRefresh();
+}
+
+boolean moveleft()
+{
+  if (space_left())
+  {
+    int i;
+    int j;
+    for (i = 0; i < 7; i++)
+    {
+      for (j = 0; j < 16; j++)
+      {
+        block[i][j] = block[i + 1][j];
+      }
+    }
+
+    for (j = 0; j < 16; j++)
+    {
+      block[7][j] = 0;
+    }
+
+    updateLED();
+    return 1;
+  }
+
+  return 0;
+}
+
+boolean moveright()
+{
+  if (space_right())
+  {
+    int i;
+    int j;
+    for (i = 7; i > 0; i--)
+    {
+      for (j = 0; j < 16; j++)
+      {
+        block[i][j] = block[i - 1][j];
+      }
+    }
+
+    for (j = 0; j < 16; j++)
+    {
+      block[0][j] = 0;
+    }
+
+    updateLED();
+    return 1;
+
+  }
+  return 0;
+}
+
+int readDirection()
+{
+  if (bdelay > millis())
+  {
+    return 0;
+  }
+  if (digitalRead(5) == 0)
+  {
+    //left
+    bdelay = millis() + btsidedelay;
+    return 3;
+  }
+
+  if (digitalRead(6) == 0)
+  {
+    //down
+    bdelay = millis() + btdowndelay;
+    return 4;
+  }
+  if (digitalRead(7) == 0)
+  {
+    //right
+    bdelay = millis() + btsidedelay;
+    return 2;
+  }
+  if (digitalRead(8) == 0)
+  {
+    //up
+    bdelay = millis() + buttondelay;
+    return 1;
+  }
+
+  return 0;
+}
+
+void updateLED()
+{
+  int i;
+  int j;
+  for (i = 0; i < 8; i++)
+  {
+    for (j = 0; j < 16; j++)
+    {
+      disp[i][j] = block[i][j] | pile[i][j];
+    }
+  }
+}
+
+void rotate()
+{
+
+  //skip for square block(3)
+  if (blocktype == 3) return;
+
+  int xi;
+  int yi;
+  int i;
+  int j;
+  //detect left
+  for (i = 7; i >= 0; i--)
+  {
+    for (j = 0; j < 16; j++)
+    {
+      if (block[i][j])
+      {
+        xi = i;
+      }
+    }
+  }
+
+  //detect up
+  for (i = 15; i >= 0; i--)
+  {
+    for (j = 0; j < 8; j++)
+    {
+      if (block[j][i])
+      {
+        yi = i;
+      }
+    }
+  }
+
+  if (blocktype == 0)
+  {
+    if (blockrotation == 0)
+    {
+
+
+      if (!space_left())
+      {
+        if (space_right3())
+        {
+          if (!moveright())
+            return;
+          xi++;
+        }
+        else return;
+      }
+      else if (!space_right())
+      {
+        if (space_left3())
+        {
+          if (!moveleft())
+            return;
+          if (!moveleft())
+            return;
+          xi--;
+          xi--;
+        }
+        else
+          return;
+      }
+      else if (!space_right2())
+      {
+        if (space_left2())
+        {
+          if (!moveleft())
+            return;
+          xi--;
+        }
+        else
+          return;
+      }
+
+
+
+
+
+      block[xi][yi] = 0;
+      block[xi][yi + 2] = 0;
+      block[xi][yi + 3] = 0;
+
+      block[xi - 1][yi + 1] = 1;
+      block[xi + 1][yi + 1] = 1;
+      block[xi + 2][yi + 1] = 1;
+
+      blockrotation = 1;
+    }
+    else
+    {
+      block[xi][yi] = 0;
+      block[xi + 2][yi] = 0;
+      block[xi + 3][yi] = 0;
+
+      block[xi + 1][yi - 1] = 1;
+      block[xi + 1][yi + 1] = 1;
+      block[xi + 1][yi + 2] = 1;
+
+      blockrotation = 0;
+    }
+  }
+
+  //offset to mid
+  xi ++;
+  yi ++;
+
+  if (blocktype == 1)
+  {
+    if (blockrotation == 0)
+    {
+      block[xi - 1][yi - 1] = 0;
+      block[xi - 1][yi] = 0;
+      block[xi + 1][yi] = 0;
+
+      block[xi][yi - 1] = 1;
+      block[xi + 1][yi - 1] = 1;
+      block[xi][yi + 1] = 1;
+
+      blockrotation = 1;
+    }
+    else if (blockrotation == 1)
+    {
+      if (!space_left())
+      {
+        if (!moveright())
+          return;
+        xi++;
+      }
+      xi--;
+
+      block[xi][yi - 1] = 0;
+      block[xi + 1][yi - 1] = 0;
+      block[xi][yi + 1] = 0;
+
+      block[xi - 1][yi] = 1;
+      block[xi + 1][yi] = 1;
+      block[xi + 1][yi + 1] = 1;
+
+      blockrotation = 2;
+    }
+    else if (blockrotation == 2)
+    {
+      yi --;
+
+      block[xi - 1][yi] = 0;
+      block[xi + 1][yi] = 0;
+      block[xi + 1][yi + 1] = 0;
+
+      block[xi][yi - 1] = 1;
+      block[xi][yi + 1] = 1;
+      block[xi - 1][yi + 1] = 1;
+
+      blockrotation = 3;
+    }
+    else
+    {
+      if (!space_right())
+      {
+        if (!moveleft())
+          return;
+        xi--;
+      }
+      block[xi][yi - 1] = 0;
+      block[xi][yi + 1] = 0;
+      block[xi - 1][yi + 1] = 0;
+
+      block[xi - 1][yi - 1] = 1;
+      block[xi - 1][yi] = 1;
+      block[xi + 1][yi] = 1;
+
+      blockrotation = 0;
+    }
+  }
+
+
+
+  if (blocktype == 2)
+  {
+    if (blockrotation == 0)
+    {
+      block[xi + 1][yi - 1] = 0;
+      block[xi - 1][yi] = 0;
+      block[xi + 1][yi] = 0;
+
+      block[xi][yi - 1] = 1;
+      block[xi + 1][yi + 1] = 1;
+      block[xi][yi + 1] = 1;
+
+      blockrotation = 1;
+    }
+    else if (blockrotation == 1)
+    {
+      if (!space_left())
+      {
+        if (!moveright())
+          return;
+        xi++;
+      }
+      xi--;
+
+      block[xi][yi - 1] = 0;
+      block[xi + 1][yi + 1] = 0;
+      block[xi][yi + 1] = 0;
+
+      block[xi - 1][yi] = 1;
+      block[xi + 1][yi] = 1;
+      block[xi - 1][yi + 1] = 1;
+
+      blockrotation = 2;
+    }
+    else if (blockrotation == 2)
+    {
+      yi --;
+
+      block[xi - 1][yi] = 0;
+      block[xi + 1][yi] = 0;
+      block[xi - 1][yi + 1] = 0;
+
+      block[xi][yi - 1] = 1;
+      block[xi][yi + 1] = 1;
+      block[xi - 1][yi - 1] = 1;
+
+      blockrotation = 3;
+    }
+    else
+    {
+      if (!space_right())
+      {
+        if (!moveleft())
+          return;
+        xi--;
+      }
+      block[xi][yi - 1] = 0;
+      block[xi][yi + 1] = 0;
+      block[xi - 1][yi - 1] = 0;
+
+      block[xi + 1][yi - 1] = 1;
+      block[xi - 1][yi] = 1;
+      block[xi + 1][yi] = 1;
+
+      blockrotation = 0;
+    }
+  }
+
+  if (blocktype == 4)
+  {
+    if (blockrotation == 0)
+    {
+      block[xi + 1][yi - 1] = 0;
+      block[xi - 1][yi] = 0;
+
+      block[xi + 1][yi] = 1;
+      block[xi + 1][yi + 1] = 1;
+
+      blockrotation = 1;
+    }
+    else
+    {
+      if (!space_left())
+      {
+        if (!moveright())
+          return;
+        xi++;
+      }
+      xi--;
+
+      block[xi + 1][yi] = 0;
+      block[xi + 1][yi + 1] = 0;
+
+      block[xi - 1][yi] = 1;
+      block[xi + 1][yi - 1] = 1;
+
+      blockrotation = 0;
+    }
+  }
+
+
+  if (blocktype == 5)
+  {
+    if (blockrotation == 0)
+    {
+      block[xi][yi - 1] = 0;
+      block[xi - 1][yi] = 0;
+      block[xi + 1][yi] = 0;
+
+      block[xi][yi - 1] = 1;
+      block[xi + 1][yi] = 1;
+      block[xi][yi + 1] = 1;
+
+      blockrotation = 1;
+    }
+    else if (blockrotation == 1)
+    {
+      if (!space_left())
+      {
+        if (!moveright())
+          return;
+        xi++;
+      }
+      xi--;
+
+      block[xi][yi - 1] = 0;
+      block[xi + 1][yi] = 0;
+      block[xi][yi + 1] = 0;
+
+      block[xi - 1][yi] = 1;
+      block[xi + 1][yi] = 1;
+      block[xi][yi + 1] = 1;
+
+      blockrotation = 2;
+    }
+    else if (blockrotation == 2)
+    {
+      yi --;
+
+      block[xi - 1][yi] = 0;
+      block[xi + 1][yi] = 0;
+      block[xi][yi + 1] = 0;
+
+      block[xi][yi - 1] = 1;
+      block[xi - 1][yi] = 1;
+      block[xi][yi + 1] = 1;
+
+      blockrotation = 3;
+    }
+    else
+    {
+      if (!space_right())
+      {
+        if (!moveleft())
+          return;
+        xi--;
+      }
+      block[xi][yi - 1] = 0;
+      block[xi - 1][yi] = 0;
+      block[xi][yi + 1] = 0;
+
+      block[xi][yi - 1] = 1;
+      block[xi - 1][yi] = 1;
+      block[xi + 1][yi] = 1;
+
+      blockrotation = 0;
+    }
+  }
+
+  if (blocktype == 6)
+  {
+    if (blockrotation == 0)
+    {
+      block[xi - 1][yi - 1] = 0;
+      block[xi][yi - 1] = 0;
+
+      block[xi + 1][yi - 1] = 1;
+      block[xi][yi + 1] = 1;
+
+      blockrotation = 1;
+    }
+    else
+    {
+      if (!space_left())
+      {
+        if (!moveright())
+          return;
+        xi++;
+      }
+      xi--;
+
+      block[xi + 1][yi - 1] = 0;
+      block[xi][yi + 1] = 0;
+
+      block[xi - 1][yi - 1] = 1;
+      block[xi][yi - 1] = 1;
+
+      blockrotation = 0;
+    }
+  }
+
+
+
+
+
+
+
+  //if rotating made block and pile overlap, push rows up
+  while (!check_overlap())
+  {
+    for (i = 0; i < 18; i++)
+    {
+      for (j = 0; j < 8; j++)
+      {
+        block[j][i] = block[j][i + 1];
+      }
+    }
+    delays = millis() + delay_;
+  }
+
+
+  updateLED();
+
+
+
+}
+
+void movedown()
+{
+  if (space_below())
+  {
+    //move down
+    int i;
+    for (i = 15; i >= 0; i--)
+    {
+      int j;
+      for (j = 0; j < 8; j++)
+      {
+        block[j][i] = block[j][i - 1];
+      }
+    }
+    for (i = 0; i < 7; i++)
+    {
+      block[i][0] = 0;
+    }
+  }
+  else
+  {
+    //merge and new block
+    int i;
+    int j;
+    for (i = 0; i < 8; i++)
+    {
+      for (j = 0; j < 16; j++)
+      {
+        if (block[i][j])
+        {
+          pile[i][j] = 1;
+          block[i][j] = 0;
+        }
+      }
+    }
+    newBlock();
+  }
+  updateLED();
+}
+
+boolean check_overlap()
+{
+  int i;
+  int j;
+  for (i = 0; i < 16; i++)
+  {
+    for (j = 0; j < 7; j++)
+    {
+      if (block[j][i])
+      {
+        if (pile[j][i])
+          return false;
+      }
+    }
+  }
+  for (i = 16; i < 18; i++)
+  {
+    for (j = 0; j < 7; j++)
+    {
+      if (block[j][i])
+      {
+        return false;
+      }
+    }
+  }
+  return true;
+}
+
+void check_gameover()
+{
+  int i;
+  int j;
+  int cnt = 0;;
+
+  for (i = 15; i >= 0; i--)
+  {
+    cnt = 0;
+    for (j = 0; j < 8; j++)
+    {
+      if (pile[j][i])
+      {
+        cnt ++;
+      }
+    }
+    if (cnt == 8)
+    {
+      points++;
+      for (j = 0; j < 8; j++)
+      {
+        pile[j][i] = 0;
+      }
+      updateLED();
+      delay(50);
+
+      int k;
+      for (k = i; k > 0; k--)
+      {
+        for (j = 0; j < 8; j++)
+        {
+          pile[j][k] = pile[j][k - 1];
+        }
+      }
+      for (j = 0; j < 8; j++)
+      {
+        pile[j][0] = 0;
+      }
+      updateLED();
+      delay(50);
+      i++;
+
+
+
+    }
+  }
+
+
+  for (i = 0; i < 8; i++)
+  {
+    if (pile[i][0])
+      gameover();
+  }
+  return;
+}
+
+void gameover()
+{
+  pt.tune_stopscore();
+  TCCR1A = 0;
+  TCCR1B = 0;
+  TCCR2A = 0;
+  TCCR2A = 0;
+  pinMode(9, 0);
+  bool high_score = 0;
+  if (points > EEPROM.read(0)) {
+    EEPROM.write(0, points);
+    high_score = 1;
+  }
+  int i;
+  int j;
+  for (i = 0; i < 8; i++)
+  {
+    for (j = 0; j < 16; j++)
+    {
+
+      disp[i][j] = 0;
+    }
+  }
+
+
+  LEDRefresh();
+  char data1[10];
+  char data2[10];
+
+  sprintf(data1, "%i", points);
+  sprintf(data2, "%i", EEPROM.read(0));
+
+  while (true)
+  {
+    if (high_score == 1) {
+      printStringWithShift("new high score  ", 80);
+      printStringWithShift(data1, 80);
+      printStringWithShift("    ", 80);
+    }
+    else {
+      printStringWithShift("score ", 80);
+      printStringWithShift(data1, 80);
+      printStringWithShift("    ", 80);
+      printStringWithShift("highest score ", 80);
+      printStringWithShift(data2, 80);
+      printStringWithShift("    ", 80);
+    }
+  }
+
+}
+
+void newBlock()
+{
+  check_gameover();
+
+
+  blocktype = random(7);
+
+
+  if (blocktype == 0)
+    // 0
+    // 0
+    // 0
+    // 0
+  {
+    block[3][0] = 1;
+    block[3][1] = 1;
+    block[3][2] = 1;
+    block[3][3] = 1;
+  }
+
+  if (blocktype == 1)
+    // 0
+    // 0 0 0
+  {
+    block[2][0] = 1;
+    block[2][1] = 1;
+    block[3][1] = 1;
+    block[4][1] = 1;
+  }
+
+  if (blocktype == 2)
+    //     0
+    // 0 0 0
+  {
+    block[4][0] = 1;
+    block[2][1] = 1;
+    block[3][1] = 1;
+    block[4][1] = 1;
+  }
+
+  if (blocktype == 3)
+    // 0 0
+    // 0 0
+  {
+    block[3][0] = 1;
+    block[3][1] = 1;
+    block[4][0] = 1;
+    block[4][1] = 1;
+  }
+
+  if (blocktype == 4)
+    //   0 0
+    // 0 0
+  {
+    block[4][0] = 1;
+    block[5][0] = 1;
+    block[3][1] = 1;
+    block[4][1] = 1;
+  }
+
+  if (blocktype == 5)
+    //   0
+    // 0 0 0
+  {
+    block[4][0] = 1;
+    block[3][1] = 1;
+    block[4][1] = 1;
+    block[5][1] = 1;
+  }
+
+  if (blocktype == 6)
+    // 0 0
+    //   0 0
+  {
+    block[3][0] = 1;
+    block[4][0] = 1;
+    block[4][1] = 1;
+    block[5][1] = 1;
+  }
+
+  blockrotation = 0;
+}
+
+boolean space_below()
+{
+  int i;
+  int j;
+  for (i = 15; i >= 0; i--)
+  {
+    for (j = 0; j < 8; j++)
+    {
+      if (block[j][i])
+      {
+        if (i == 15)
+          return false;
+        if (pile[j][i + 1])
+        {
+          return false;
+        }
+      }
+    }
+  }
+  return true;
+}
+
+boolean space_left2()
+{
+  int i;
+  int j;
+  for (i = 15; i >= 0; i--)
+  {
+    for (j = 0; j < 8; j++)
+    {
+      if (block[j][i])
+      {
+        if (j == 0 || j == 1)
+          return false;
+        if (pile[j - 1][i] | pile[j - 2][i])
+        {
+          return false;
+        }
+      }
+    }
+  }
+  return true;
+}
+
+boolean space_left3()
+{
+  int i;
+  int j;
+  for (i = 15; i >= 0; i--)
+  {
+    for (j = 0; j < 8; j++)
+    {
+      if (block[j][i])
+      {
+        if (j == 0 || j == 1 || j == 2 )
+          return false;
+        if (pile[j - 1][i] | pile[j - 2][i] | pile[j - 3][i])
+        {
+          return false;
+        }
+      }
+    }
+  }
+  return true;
+}
+
+boolean space_left()
+{
+  int i;
+  int j;
+  for (i = 15; i >= 0; i--)
+  {
+    for (j = 0; j < 8; j++)
+    {
+      if (block[j][i])
+      {
+        if (j == 0)
+          return false;
+        if (pile[j - 1][i])
+        {
+          return false;
+        }
+      }
+    }
+  }
+  return true;
+}
+
+boolean space_right()
+{
+  int i;
+  int j;
+  for (i = 15; i >= 0; i--)
+  {
+    for (j = 0; j < 8; j++)
+    {
+      if (block[j][i])
+      {
+        if (j == 7)
+          return false;
+        if (pile[j + 1][i])
+        {
+          return false;
+        }
+      }
+    }
+  }
+  return true;
+}
+
+boolean space_right3()
+{
+  int i;
+  int j;
+  for (i = 15; i >= 0; i--)
+  {
+    for (j = 0; j < 8; j++)
+    {
+      if (block[j][i])
+      {
+        if (j == 7 || j == 6 || j == 5)
+          return false;
+        if (pile[j + 1][i] | pile[j + 2][i] | pile[j + 3][i])
+        {
+          return false;
+        }
+      }
+    }
+  }
+  return true;
+}
+
+boolean space_right2()
+{
+  int i;
+  int j;
+  for (i = 15; i >= 0; i--)
+  {
+    for (j = 0; j < 8; j++)
+    {
+      if (block[j][i])
+      {
+        if (j == 7 || j == 6)
+          return false;
+        if (pile[j + 1][i] | pile[j + 2][i])
+        {
+          return false;
+        }
+      }
+    }
+  }
+  return true;
+}
+
+void LEDRefresh()
+{
+  int i;
+  int k;
+  //////////////////////////////////////////////
+
+  for (i = 0; i < 8; i++)
+  {
+    byte upper = 0;
+    int b;
+    for (b = 0; b < 8; b++)
+    {
+      upper <<= 1;
+      if (disp[b][i]) upper |= 1;
+    }
+
+    lc.setRow(1, i, upper);
+
+
+
+    byte lower = 0;
+    for (b = 0; b < 8; b++)
+    {
+      lower <<= 1;
+      if (disp[b][i + 8]) lower |= 1;
+    }
+
+    lc.setRow(0, i, lower);
+
+   
+  }
+}
+void printCharWithShift(char c, int shift_speed) {   
+  if (c < 32) return;
+  c -= 32;
+  memcpy_P(buffer, CH + 7 * c, 7);
+  lc.writeSprite(DISPLAYS * 8, 0, buffer);
+  lc.setColumn(DISPLAYS * 8 + buffer[0], 0);
+
+  for (int i = 0; i < buffer[0] + 1; i++)
+  {
+    delay(shift_speed);
+    lc.shiftLeft(false, false);
+  }
+}
+
+void printStringWithShift(char* s, int shift_speed) {      
+  while (*s != 0) {
+    printCharWithShift(*s, shift_speed);
+    s++;
+  }
+}
+
+void printString(char* s)                       
+{
+  int col = 0;
+  while (*s != 0)
+  {
+    if (*s < 32) continue;
+    char c = *s - 32;
+    memcpy_P(buffer, CH + 7 * c, 7);
+    lc.writeSprite(col, 0, buffer);
+    lc.setColumn(col + buffer[0], 0);
+    col += buffer[0] + 1;
+    s++;
+  }
+}
